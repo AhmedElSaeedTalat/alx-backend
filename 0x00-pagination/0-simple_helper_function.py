a0ffc028349264a58ee0e4670e7e20e695181eff
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-27 11:16:46 +0000

    Task 0 help

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments page and page_size.
+
+"""
+
+
+def index_range(page, page_size):
+    """ """
+    return (page, page_size)
