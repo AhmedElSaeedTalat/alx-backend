8b750e09c6096a6c93cdff53c8bc9ce56d8d3426
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-27 11:42:37 +0000

    Task 0 help

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,10 +3,17 @@
 function named index_range that takes two integer arguments page and page_size.
 
 """
+from typing import Tuple
 
 
-def index_range(page, page_size):
-    """ """
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+        function to calculate start and end indeces
+        Args:
+            page: page number
+            page_size: page size
+        Return - start index, end index in a tuple
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
