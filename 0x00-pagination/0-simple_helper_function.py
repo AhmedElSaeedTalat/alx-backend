9766dafdd6e1c37abd45a2e4915322bc3173adf1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-27 11:40:22 +0000

    Task 0 help

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,4 +7,6 @@ function named index_range that takes two integer arguments page and page_size.
 
 def index_range(page, page_size):
     """ """
-    return (page, page_size)
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
