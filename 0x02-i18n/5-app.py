5296e6f2bfbd6ad0662f580f704909e359c4c41c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-06 23:35:39 +0000

    task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """ run app module """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
@@ -27,10 +28,10 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ return locale """
-    if g.user and g.user['locale'] in Config.LANGUAGES:
-        return g.user['locale']
     if 'locale' in request.args:
         return request.args['locale']
+    elif g.user and g.user['locale'] in Config.LANGUAGES:
+        return g.user['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
