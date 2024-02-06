df2e241269d1317fa347846320d8ee60a367f3e4
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-06 23:36:24 +0000

    task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -38,7 +38,7 @@ def get_locale():
 def get_user() -> Union[Dict[str, str], None]:
     """ gets user """
     if 'login_as' in request.args:
-        return users.get(int(request.args['login_as']))
+        return users.get(int(request.args['login_as']), 0)
 
 
 @app.before_request
