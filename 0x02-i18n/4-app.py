c1b3495971e9ef063664d10016a512ae4a1b469b
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-06 21:58:26 +0000

    task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" run app module """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ class config fot babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ return locale """
+    if 'locale' in request.args:
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello() -> str:
+    """ home page """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
