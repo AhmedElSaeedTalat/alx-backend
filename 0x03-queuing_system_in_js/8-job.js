64f865e1e55bbe03b3e81d97c42ad9a9f5398f47
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-03-06 15:33:25 +0000

    task 8 create a job

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,26 @@
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+  for (let i = 0; i < jobs.length; i++) {
+    const job = queue.create('push_notification_code_3', jobs[i]);
+    job.save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+	});
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`)
+	});
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed ${err}`)
+	});
+
+    job.on('progress', (percent) => {
+      console.log(`Notification job ${job.id} ${percent}% complete`);
+	});
+  }
+};
+module.exports = createPushNotificationsJobs;
